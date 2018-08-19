679d783bdc8620a8ab02ee117064a6a83716afc1
Stancu Florin <niflostancu@gmail.com>
2018-08-19 21:59:23 +0300

    test for the services supervision + waiting script

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Base image tests
 
-TESTS=(base)
+TESTS=(base services)
 
 if [[ -n "$DUMMY_TESTS" ]]; then
-    TESTS=(base dummy fail)
+    TESTS=(base services dummy fail)
 fi
 
 # declare the image to be tested
 TEST_IMAGE=${TEST_IMAGE:-nicloud/server-base}
+
+SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 function test_base_before() {
     DOCKER_ARGS+=(-e "PUID=1001" -e "PGID=1002")
@@ -17,6 +19,17 @@ function test_base() {
     # base image testing
     dtest_exec wait-for-services
     dtest_goss "test_base.yaml"
+}
+
+function test_services_before() {
+    # add a sample services as readonly volumes
+    DOCKER_ARGS+=(-v "$SDIR/service-sampled:/etc/services.d/sampled:ro")
+    DOCKER_ARGS+=(-v "$SDIR/service-down:/etc/services.d/downd:ro")
+    DOCKER_ARGS+=(-e "S6_READ_ONLY_ROOT=1")
+}
+function test_services() {
+    dtest_exec wait-for-services
+    dtest_goss "test_services.yaml"
 }
 
 function test_dummy() {
@@ -29,7 +42,6 @@ function test_fail() {
 }
 
 # Source the testing framework
-SDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 . "$SDIR/_dtest.sh"
 
 # run the tests!
